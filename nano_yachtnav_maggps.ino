41fd196ca7b29434d8dbb4fc90cdfe378e5601ff
Andrew serykh <andrew.serykh@gmail.com>
2019-04-25 14:29:30 +0700

    Simple link protocol 2 directions
    Простой протокол связи esp и nano взамен modbus.
    Управление 4-мя релейными выходами.

--- nano_yachtnav_maggps.ino
+++ nano_yachtnav_maggps.ino
@@ -1,84 +1,60 @@
-/* v2.02
+/* v2.10
 
-Прием I2C данных компаса и данных GPS и передача по MODBUS RTU
-с фильтрацией компаса
+Простой протокол связи обмен структурой данных
+Функции управления выходными реле
+Прием I2C данных компаса и данных GPS
 
 TX, RX - связь с esp32
 D10, D11 (RX,TX) - подключение GPS
 A4, A5 - компас
 LED - прием пакетов с GPS
-
-Таблица регистров:
-
-00- compass course.deg()
-01- (int)
-
-02- gps.course.deg();
-03- (int)
-
-04- gps.speed.kmph()
-05- (int)
-
-06- gps.speed.knots()
-07- (double)
-08- 
-09-
-
-10- gps.location.lat()
-11- (double)
-12- 
-13- 
-
-14- gps.location.lng()
-15-
-16-
-17-
-
-18- gps.altitude.meters()
-19- (int)
-
-20- gps.time.hour()
-21-
-
-22- gps.time.minute()
-23-
-
-24- gps.time.second()
-25-
-
-26- gps.date.day()
-27-
-
-28- gps.date.month()
-29-
-
-30- gps.date.year()
-31-
-
 */
+
 #include <SoftwareSerial.h>
 #include "HMC5883.h"
 #include "TinyGPSplus.h"
-#include "ModbusRtu.h"
 
 #define BAUD            9600  //Скорость последовательного порта
-#define MBADDR          1     //MODBUS Адрес (0-127)
-#define TXEN            6     //RS485 RE-DE
 #define LED             13    //Светоиод на Arduino
 
 #define COMPASS_COUNT   5     //Количество измерений для усреднения
 #define COMPASS_FILTER  10    //Максимальное отклонение для отброса показаний
 
+#define T_MS_RECV       100   //Оттяжка ожидания принятого пакета
+#define T_MS_SEND       500   //Интервал отправки данных на esp
+
+#define RELAY_IN1       2     //Выходы на управление актуаторами через релейный модуль
+#define RELAY_IN2       3     //на 4 реле
+#define RELAY_IN3       4
+#define RELAY_IN4       5
+
 HMC5883 compass;
 SoftwareSerial SSerialGPS(7, 8); // RX, TX [10,11]
 TinyGPSPlus gps_parser;
 
-//для поиска тега раскомментировать тут и в программе область заполнения таблицы регистров
-//тогда значения в тегах будут 1,2,3,4 и таким образом можно определить куда "упал" какой канал
-//uint16_t mbdata[32] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, ,23 ,24, 25, 26, 27, 28, 29, 30, 31, 32 };
-uint16_t mbdata[32] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-Modbus slave(MBADDR, 0, TXEN);
-int8_t state = 0;   //Статус MB
+struct { //Структура пакета данных измерений передаваемых из nano в esp
+  byte Header; //заголовок пакета
+  byte Length; //длина пакета
+  uint16_t CompassCourse;
+  uint16_t GpsCourse;
+  uint16_t GpsSpeedKmh;
+  float GpsSpeedKnots;
+  float GpsLat;
+  float GpsLng;
+  uint16_t GpsAlt;
+  uint16_t GpsHour;
+  uint16_t GpsMinute;
+  uint16_t GpsSecond;
+  uint16_t GpsDay;
+  uint16_t GpsMonth;
+  uint16_t GpsYear;
+} datanano;
+
+struct { //Структура пакета команд передаваемых из esp в nano
+  byte Header;
+  byte Length;
+  byte Relay;
+} dataesp;
 
 int compass_x,compass_y,compass_z;
 float compass_hdg;
@@ -86,49 +62,40 @@ float compass_hdg_avg; //среднее
 float compass_hdg_arr[COMPASS_COUNT];
 int compass_arr_cnt=0;
 
+char SerialEspIn[64];    //буфер приема 
+byte SerialEspInLen;     //заполнение буфера
+
 long ms_compass;
 long ms_gps;
+long ms_recv; //оттяжка для приема пакета
+long ms_send; //счетчик для отправки данных в порт
 
 void setup() {
-  pinMode(2,OUTPUT);    //D2
-  pinMode(4,OUTPUT);    //D4
-  pinMode(12,OUTPUT);   //D12
-  pinMode(LED, OUTPUT); //D13 LED
+  pinMode(RELAY_IN1,OUTPUT);
+  pinMode(RELAY_IN2,OUTPUT);
+  pinMode(RELAY_IN3,OUTPUT);
+  pinMode(RELAY_IN4,OUTPUT);
   
-  pinMode(A7,INPUT);
+  pinMode(LED, OUTPUT); //D13 LED
+
   //Подсвечиваем запуск контроллера диодом
   digitalWrite(LED, HIGH);
   delay(1000);
   digitalWrite(LED, LOW);
   delay(1000);
 
-//  Serial.begin(9600);
-  SSerialGPS.begin(9600);
-  
-  slave.begin( BAUD );
-
-  //Высвечиваем диодом MB адрес устройства
-  for (int i=0; i<MBADDR; i++){
-    digitalWrite(LED, HIGH); delay(200);
-    digitalWrite(LED, LOW); delay(200);
-  }
-  delay(500);
+  Serial.begin(9600);
+  SSerialGPS.begin(9600);  
 
   compass.init();
-
-  ms_compass = ms_gps = millis();
+  ms_compass = ms_gps = ms_send = millis();
 
   compass.read(&compass_x,&compass_y,&compass_z,&compass_hdg);
   for (int i=0; i<COMPASS_COUNT; i++) compass_hdg_arr[i]=compass_hdg;
-  
+
 }
 
 void loop() {
-
-  state = slave.poll( mbdata, 32 ); //Обработчик MODBUS
-
-  digitalWrite(2,(digitalRead(A7)));
-  //if ( digitalRead(A7) ) digitalWrite(2,!(digitalRead(2)));
 
   if (millis() - ms_compass > 50) {
     compass.read(&compass_x,&compass_y,&compass_z,&compass_hdg);
@@ -137,15 +104,10 @@ void loop() {
       compass_arr_cnt++;
     //}
     if (compass_arr_cnt > COMPASS_COUNT) compass_arr_cnt=0;
-    
     compass_hdg_avg=0;
     for (int i=0; i<COMPASS_COUNT; i++) compass_hdg_avg+=compass_hdg_arr[i];
     compass_hdg_avg = compass_hdg_avg/COMPASS_COUNT;
-
-    //Serial.print(compass_hdg);
-    //Serial.print(" ");
-    //Serial.println(compass_hdg_avg);
-    
+    datanano.CompassCourse = (uint16_t)compass_hdg_avg;    
     ms_compass = millis();
   } //ms_compass
 
@@ -158,38 +120,47 @@ void loop() {
   }
 
   if(gps_parser.time.isValid()) {
-    union {
-      double u_double;
-      uint16_t u_ints[2];
-    };
-    mbdata[0] = (uint16_t)compass_hdg_avg;
-    mbdata[1] = (uint16_t)gps_parser.course.deg();
-    mbdata[2] = (uint16_t)gps_parser.speed.kmph();
-    
-    u_double = gps_parser.speed.knots();
-    mbdata[3] = u_ints[0];
-    mbdata[4] = u_ints[1];
-
-    u_double = gps_parser.location.lat();
-    mbdata[5] = u_ints[0];
-    mbdata[6] = u_ints[1];
-
-    u_double = gps_parser.location.lng();
-    mbdata[7] = u_ints[0];
-    mbdata[8] = u_ints[1];
-
-  Serial.println(gps_parser.location.lng());
-  Serial.println(gps_parser.location.lat());
-        
-    mbdata[9] = (uint16_t)gps_parser.altitude.meters();
-
-    mbdata[10] = (uint16_t)gps_parser.time.hour();
-    mbdata[11] = (uint16_t)gps_parser.time.minute();
-    mbdata[12] = (uint16_t)gps_parser.time.second();
-    mbdata[13] = (uint16_t)gps_parser.date.day();
-    mbdata[14] = (uint16_t)gps_parser.date.month();
-    mbdata[15] = (uint16_t)gps_parser.date.year();
-   
+    datanano.GpsCourse = (uint16_t)gps_parser.course.deg();
+    datanano.GpsSpeedKmh = (uint16_t)gps_parser.speed.kmph();    
+    datanano.GpsSpeedKnots = gps_parser.speed.knots();
+    datanano.GpsLat = gps_parser.location.lat();
+    datanano.GpsLng = gps_parser.location.lng();
+    datanano.GpsAlt = (uint16_t)gps_parser.altitude.meters();
+    datanano.GpsHour = (uint16_t)gps_parser.time.hour();
+    datanano.GpsMinute = (uint16_t)gps_parser.time.minute();
+    datanano.GpsSecond = (uint16_t)gps_parser.time.second();
+    datanano.GpsDay = (uint16_t)gps_parser.date.day();
+    datanano.GpsMonth = (uint16_t)gps_parser.date.month();
+    datanano.GpsYear = (uint16_t)gps_parser.date.year();
   }
+
+  if (millis() - ms_send > T_MS_SEND) {
+    datanano.Header = 0x65;
+    datanano.Length = (byte)sizeof(datanano);
+    for (int i=0; i<sizeof(datanano); i++) Serial.write( ((char*)&datanano)[i] );
+    ms_send = millis();
+  }
+
+  while (Serial.available() > 0) {
+    char SerialChar = (char)Serial.read();
+    SerialEspIn[SerialEspInLen] = SerialChar;
+    SerialEspInLen++;
+    ms_recv = millis();
+  }
+  
+  if (SerialEspInLen > 0 && (millis() - ms_recv > T_MS_RECV)) {    
+   Serial.println("Packet!");
+   for (int q = 0; q < SerialEspInLen; q++) { Serial.print((byte)SerialEspIn[q], HEX); Serial.print(" "); } Serial.println(" ");
+  
+   if (SerialEspIn[0] == 0x69){ //Проверка заголовка пакета 0x69
+    if (sizeof(dataesp)>=SerialEspInLen) for (int i=0; i<SerialEspInLen; i++) ((byte*)&dataesp)[i] = SerialEspIn[i]; //загрузка структуры данных
+   }
+   SerialEspInLen=0;
+  }
+
+  digitalWrite( RELAY_IN1, bitRead( dataesp.Relay, 0 ));
+  digitalWrite( RELAY_IN2, bitRead( dataesp.Relay, 1 ));
+  digitalWrite( RELAY_IN3, bitRead( dataesp.Relay, 2 ));
+  digitalWrite( RELAY_IN4, bitRead( dataesp.Relay, 3 ));
 
 } //loop
