8736b6d4c2a0fec75db526d6f5e1137a8d111561
Andrew Serykh <andrew.serykh@gmail.com>
2019-05-06 23:20:18 +0700

    WDT

--- nano_yachtnav_maggps.ino
+++ nano_yachtnav_maggps.ino
@@ -1,5 +1,6 @@
-/* v2.11
+/* v2.30
 
+Watchdog таймер
 Простой протокол связи обмен структурой данных
 Функции управления выходными реле
 Прием I2C данных компаса и данных GPS
@@ -10,6 +11,7 @@ A4, A5 - компас
 LED - прием пакетов с GPS
 */
 
+#include <avr/wdt.h>
 #include <SoftwareSerial.h>
 #include "HMC5883.h"
 #include "TinyGPSplus.h"
@@ -20,8 +22,9 @@ LED - прием пакетов с GPS
 #define COMPASS_COUNT   5     //Количество измерений для усреднения
 #define COMPASS_FILTER  10    //Максимальное отклонение для отброса показаний
 
-#define T_MS_RECV       100   //Оттяжка ожидания принятого пакета
+#define T_MS_COMPASS    500    //Интервал опроса компаса
 #define T_MS_SEND       500   //Интервал отправки данных на esp
+#define T_MS_RECV       100   //Оттяжка ожидания принятого пакета
 
 #define RELAY_IN1       2     //Выходы на управление актуаторами через релейный модуль
 #define RELAY_IN2       3     //на 4 реле
@@ -62,7 +65,7 @@ float compass_hdg_avg; //среднее
 float compass_hdg_arr[COMPASS_COUNT];
 int compass_arr_cnt=0;
 
-char SerialEspIn[64];    //буфер приема 
+char SerialEspIn[256];    //буфер приема 
 byte SerialEspInLen;     //заполнение буфера
 
 long ms_compass;
@@ -71,53 +74,34 @@ long ms_recv; //оттяжка для приема пакета
 long ms_send; //счетчик для отправки данных в порт
 
 void setup() {
+  wdt_disable();
+  pinMode(LED, OUTPUT); //D13 LED
+  //digitalWrite(LED, HIGH);
+
   pinMode(RELAY_IN1,OUTPUT);
   pinMode(RELAY_IN2,OUTPUT);
   pinMode(RELAY_IN3,OUTPUT);
-  pinMode(RELAY_IN4,OUTPUT);
-  
-  pinMode(LED, OUTPUT); //D13 LED
-
-  //Подсвечиваем запуск контроллера диодом
-  digitalWrite(LED, HIGH);
-  delay(1000);
-  digitalWrite(LED, LOW);
-  delay(1000);
+  pinMode(RELAY_IN4,OUTPUT);  
 
   Serial.begin(9600);
   SSerialGPS.begin(9600);  
 
+  wdt_enable(WDTO_4S);  
+
   compass.init();
+  //compass.read(&compass_x,&compass_y,&compass_z,&compass_hdg);
+  //for (int i=0; i<COMPASS_COUNT; i++) compass_hdg_arr[i]=compass_hdg;
+
+  digitalWrite(RELAY_IN1, LOW);
+  digitalWrite(RELAY_IN2, LOW);
+  digitalWrite(RELAY_IN3, LOW);
+  digitalWrite(RELAY_IN4, LOW);
+  //digitalWrite(LED, LOW);  
   ms_compass = ms_gps = ms_send = millis();
-
-  compass.read(&compass_x,&compass_y,&compass_z,&compass_hdg);
-  for (int i=0; i<COMPASS_COUNT; i++) compass_hdg_arr[i]=compass_hdg;
-
+  wdt_reset();
 }
 
 void loop() {
-
-  if (millis() - ms_compass > 50) {
-    compass.read(&compass_x,&compass_y,&compass_z,&compass_hdg);
-    //if (abs(compass_hdg-compass_hdg_avg) <= COMPASS_FILTER) {
-      compass_hdg_arr[compass_arr_cnt] = compass_hdg;
-      compass_arr_cnt++;
-    //}
-    if (compass_arr_cnt > COMPASS_COUNT) compass_arr_cnt=0;
-    compass_hdg_avg=0;
-    for (int i=0; i<COMPASS_COUNT; i++) compass_hdg_avg+=compass_hdg_arr[i];
-    compass_hdg_avg = compass_hdg_avg/COMPASS_COUNT;
-    datanano.CompassCourse = (uint16_t)compass_hdg_avg;    
-    ms_compass = millis();
-  } //ms_compass
-
-
-  while (SSerialGPS.available()) {
-    digitalWrite(LED, HIGH);
-    char temp = SSerialGPS.read();
-    gps_parser.encode(temp);
-    digitalWrite(LED, LOW);
-  }
 
   if(gps_parser.time.isValid()) {
     datanano.GpsCourse = (uint16_t)gps_parser.course.deg();
@@ -141,17 +125,23 @@ void loop() {
     ms_send = millis();
   }
 
+  while (SSerialGPS.available()) {
+    digitalWrite(LED, HIGH);
+    char temp = SSerialGPS.read();
+    gps_parser.encode(temp);
+    digitalWrite(LED, LOW);
+  }
+
   while (Serial.available() > 0) {
     char SerialChar = (char)Serial.read();
     SerialEspIn[SerialEspInLen] = SerialChar;
     SerialEspInLen++;
+    if (SerialEspInLen>255) SerialEspInLen=0;
     ms_recv = millis();
   }
   
   if (SerialEspInLen > 0 && (millis() - ms_recv > T_MS_RECV)) {    
-   Serial.println("Packet!");
-   for (int q = 0; q < SerialEspInLen; q++) { Serial.print((byte)SerialEspIn[q], HEX); Serial.print(" "); } Serial.println(" ");
-  
+   //for (int q = 0; q < SerialEspInLen; q++) { Serial.print((byte)SerialEspIn[q], HEX); Serial.print(" "); } Serial.println(" ");
    if (SerialEspIn[0] == 0x69){ //Проверка заголовка пакета 0x69
     if (sizeof(dataesp)>=SerialEspInLen) for (int i=0; i<SerialEspInLen; i++) ((byte*)&dataesp)[i] = SerialEspIn[i]; //загрузка структуры данных
    }
@@ -162,5 +152,25 @@ void loop() {
   digitalWrite( RELAY_IN2, !bitRead( dataesp.Relay, 1 ));
   digitalWrite( RELAY_IN3, !bitRead( dataesp.Relay, 2 ));
   digitalWrite( RELAY_IN4, !bitRead( dataesp.Relay, 3 ));
+
+  if (millis() - ms_compass > T_MS_COMPASS) {
+    digitalWrite(LED, HIGH);
+    compass.read(&compass_x,&compass_y,&compass_z,&compass_hdg);
+    /*
+    compass_hdg_arr[compass_arr_cnt] = compass_hdg;
+    compass_arr_cnt++;
+    if (compass_arr_cnt => COMPASS_COUNT) compass_arr_cnt=0;
+    compass_hdg_avg=0;
+    for (int i=0; i<COMPASS_COUNT; i++) compass_hdg_avg+=compass_hdg_arr[i];
+    compass_hdg_avg = compass_hdg_avg/COMPASS_COUNT;
+    */
+    compass_hdg_avg = compass_hdg;
+    datanano.CompassCourse = (uint16_t)compass_hdg_avg;
+    delay(50);
+    ms_compass = millis();
+    digitalWrite(LED, LOW);
+  } //ms_compass
+  
+  wdt_reset();
 
 } //loop
